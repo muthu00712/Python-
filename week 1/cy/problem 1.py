e791ad0773c674ee0dc8950e87f34c5deb7ff85a
muthu00712 <muthukutti.r.2024.aids@rajalakshmi.edu.in>
2025-06-08 16:27:59 +0530

    week 1

---
+++ week 1/cy/problem 1.py
@@ -0,0 +1,10 @@
+a=int(input())
+b=a*2
+c=0
+if a>=10:
+    c=b+5
+else:
+    c=max(c,20)
+d=c*25
+print("Number of tacos needed: ",c)
+print("Total cost: ",d)
---
+++ week 1/cy/problem 3.py
@@ -0,0 +1,8 @@
+a=int(input())
+b=int(input())
+c=int(input())
+d=a&b
+e=d^c
+f=e*3
+g=f%1000
+print(g)
