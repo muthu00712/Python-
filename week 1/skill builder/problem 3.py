698784b170180fbc4fb17d0798af2ec480d55104
muthu00712 <muthukutti.r.2024.aids@rajalakshmi.edu.in>
2025-06-08 16:23:51 +0530

    week 1

---
+++ week 1/skill builder/problem 3.py
@@ -0,0 +1,10 @@
+a=int(input())
+b=float(input())
+print("Initial salaries:")
+print("Alice's salary = ",a)
+print("Bob's salary = ",b)
+c=a
+a=b
+print("\nNew salaries after swapping:")
+print("Alice's salary = ",a)
+print("Bob's salary = ",c)
